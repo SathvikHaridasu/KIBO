ba0b41f05a19083b17a7d7fecd75876b50e5f29c
Sathvik Haridasu <sathviksh6@gmail.com>
2025-09-13 08:17:34 -0400

    client detection

---
+++ detection_client.js
@@ -0,0 +1,9 @@
+// detection_client.js - Stub for detection client
+
+function startDetection() {
+  console.log('Detection started');
+}
+
+function stopDetection() {
+  console.log('Detection stopped');
+}
